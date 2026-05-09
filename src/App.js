0624df1fae8396708de7524bbd2b0509a78efa38
SGibson-12 <samcgib@gmail.com>
2026-05-09 18:21:35 -0500

    Update on homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sam Gibson Project 4 - React App
+          Sam Gibson Project 4 w/ a change - React App
         </p>
         <a
           className="App-link"
